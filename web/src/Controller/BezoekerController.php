59b6bb4798a171774c09bc364179756adb991bab
AbdulH <haritani2002@gmail.com>
2022-02-04 13:07:29 +0100

    everything working week 2

--- web/src/Controller/BezoekerController.php
+++ web/src/Controller/BezoekerController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
 
 class BezoekerController extends AbstractController
 {
     /**
      * @Route("/bezoeker", name="bezoeker")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('bezoeker/index.html.twig', [
-            'controller_name' => 'BezoekerController',
-        ]);
+        return $this->render('bezoeker/index.html.twig'
+        );
     }
+
+       /**
+     * @Route("/category", name="category_index", methods={"GET"})
+     */
+    public function categoriesAction()
+    {
+        $categories=$this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findAll();
+        return $this->render('bezoeker/category.html.twig',['categories'=>$categories]);
+    }
+     /**
+     * @Route("/category/{id}", name="category", methods={"GET"})
+     */
+    public function catergoryActions($id)
+    {
+       $category=$this->getDoctrine()
+       ->getRepository(Category::class)
+       ->find($id);
+       return $this->render('bezoeker/product.html.twig',['category'=>$category]);
+    }
+
 }
